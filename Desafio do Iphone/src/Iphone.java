033b78a050e7dac9e0c859a20834a97e943260fd
varage <lucashanauer21@gmail.com>
2025-09-08 14:23:31 -0300

    Adicionado Diagrama do Iphone feito pelo Astah

---
+++ Desafio do Iphone/src/Iphone.java
@@ -0,0 +1,51 @@
+public class Iphone extends Iphone.ReprodutorMusical, Iphone.AparelhoTelefonico, Iphone.NavegadorInternet {
+
+	public class NavegadorInternet {
+
+		public void exibirPagina(int String url) {
+
+		}
+
+		public void adicionarNovaAba() {
+
+		}
+
+		public void atualizarPagina() {
+
+		}
+
+	}
+
+	public class ReprodutorMusical {
+
+		public void tocar() {
+
+		}
+
+		public void pausar() {
+
+		}
+
+		public void selecionarMusica(int String musica) {
+
+		}
+
+	}
+
+	public class AparelhoTelefonico {
+
+		public void ligar(int String numero) {
+
+		}
+
+		public void atender() {
+
+		}
+
+		public void iniciarCorreioVoz() {
+
+		}
+
+	}
+
+}
